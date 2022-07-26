139ae1c87b6bcffc6db78e3b580c95ad9bb0206f
jessica-calderon <calderonjessica13@yahoo.com>
2022-07-26 00:27:03 -0500

    login logout

--- server.js
+++ server.js
@@ -5,6 +5,20 @@ const sequelize = require('./config/connection');
 // set up Handlebars as the template engine
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
 
 
 const app = express();
@@ -15,6 +29,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session(sess));
 // turn on routes
 app.use(routes);
 
